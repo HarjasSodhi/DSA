6c99997a6869091ca68e28b9878c77c451195140
HarjasSodhi <harjassodhi@yahoo.in>
2021-12-27 02:15:52 +0530

    update

--- Level-Up/StackAndQueue/NextGreaterSet.java
+++ Level-Up/StackAndQueue/NextGreaterSet.java
@@ -1,6 +1,7 @@
 import java.util.Stack;
-import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 //classLink= https://classroom.pepcoding.com/the-placement-program-pitampura-mar-14-2021/116/classvideos/5533
 public class NextGreaterSet {
@@ -286,6 +287,7 @@ public class NextGreaterSet {
         return ans;
     }
 
+    // leetcode 85
     public int maximalRectangle(char[][] matrix) {
         int ans = 0;
         int n = matrix.length, m = matrix[0].length;
@@ -303,6 +305,7 @@ public class NextGreaterSet {
         return ans;
     }
 
+    // leetcode 32
     public int longestValidParentheses(String s) {
         int ans = 0;
         Stack<Integer> st = new Stack<>();
@@ -318,6 +321,120 @@ public class NextGreaterSet {
         }
 
         return ans;
+    }
+
+    // leetcode 402
+    public String removeKdigits(String num, int k) {
+        int n = num.length();
+        if (k == n)
+            return "0";
+        ArrayList<Character> st = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            while (st.size() != 0 && st.get(st.size() - 1) > num.charAt(i) && k > 0) {
+                // do not pop on st.get(st.size() - 1) >= num.charAt(i) test case -> num=112 k=1
+                k--;
+                st.remove(st.size() - 1);
+            }
+            st.add(num.charAt(i));
+        }
+        while (k-- > 0) {
+            st.remove(st.size() - 1);
+        }
+        boolean flag = false;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < st.size(); i++) {
+            char ch = st.get(i);
+            if (ch != '0') {
+                flag = true;
+            }
+            if (flag) {
+                sb.append(ch);
+            }
+        }
+        return sb.length() != 0 ? sb.toString() : "0";
+    }
+
+    // leetcode 316
+    public String removeDuplicateLetters(String s) {
+        ArrayList<Character> st = new ArrayList<>();
+        boolean[] arr = new boolean[26];
+        int[] freq = new int[26];
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+        Arrays.fill(arr, false);
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']--;
+            while (st.size() != 0 && ch < st.get(st.size() - 1) && !arr[ch - 'a']
+                    && freq[st.get(st.size() - 1) - 'a'] > 0) {
+                char temp = st.remove(st.size() - 1);
+                arr[temp - 'a'] = false;
+            }
+            if (!arr[ch - 'a']) {
+                st.add(ch);
+                arr[ch - 'a'] = true;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < st.size(); i++) {
+            char ch = st.get(i);
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+
+    // leetcode 1081
+    public String smallestSubsequence(String s) {
+        ArrayList<Character> st = new ArrayList<>();
+        boolean[] arr = new boolean[26];
+        int[] freq = new int[26];
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+        Arrays.fill(arr, false);
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']--;
+            while (st.size() != 0 && ch < st.get(st.size() - 1) && !arr[ch - 'a']
+                    && freq[st.get(st.size() - 1) - 'a'] > 0) {
+                char temp = st.remove(st.size() - 1);
+                arr[temp - 'a'] = false;
+            }
+            if (!arr[ch - 'a']) {
+                st.add(ch);
+                arr[ch - 'a'] = true;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < st.size(); i++) {
+            char ch = st.get(i);
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+
+    // leetcode 1249
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (st.peek() != -1 && ch == ')') {
+                sb.append(')');
+                st.pop();
+            } else if (ch != ')') {
+                sb.append(ch);
+                if (ch == '(')
+                    st.push(sb.length() - 1);
+            }
+        }
+
+        while (st.peek() != -1) {
+            sb.deleteCharAt(st.pop());
+        }
+
+        return sb.toString();
     }
 
 }
