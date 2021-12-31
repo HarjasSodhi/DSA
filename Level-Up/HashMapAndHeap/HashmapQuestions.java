01e771cb07e4a7224a84c4220baabf3b677a313d
HarjasSodhi <harjassodhi@yahoo.in>
2021-12-31 20:24:48 +0530

    update

---
+++ Level-Up/HashMapAndHeap/HashmapQuestions.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class HashmapQuestions {
+    // 692
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> {
+            if (map.get(a) == map.get(b)) {
+                return b.compareTo(a);
+            }
+            return map.get(a) - map.get(b);
+        });
+
+        for (String s : map.keySet()) {
+            pq.add(s);
+            if (pq.size() > k) {
+                pq.remove();
+            }
+        }
+
+        List<String> ans = new LinkedList<>();
+        while (pq.size() != 0) {
+            ans.add(0, pq.remove());
+        }
+        return ans;
+    }
+
+    // 128
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> map = new HashSet<>();
+        for (int ele : nums)
+            map.add(ele);
+
+        int len = 0;
+        for (int ele : nums) {
+            if (!map.contains(ele))
+                continue;
+
+            int prev = ele - 1, next = ele + 1;
+            map.remove(ele);
+            while (map.contains(prev))
+                map.remove(prev--);
+            while (map.contains(next))
+                map.remove(next++);
+
+            len = Math.max(len, next - prev - 1);
+        }
+
+        return len;
+    }
+
+}
--- Level-Up/HashMapAndHeap/HeapQuestions.java
+++ Level-Up/HashMapAndHeap/HeapQuestions.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
-public class Questions {
+public class HeapQuestions {
     // https://classroom.pepcoding.com/the-placement-program-pitampura-mar-14-2021/116/classvideos/5687
 
     // https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/1
     // nlog(k)
     // you can also use your own heap class and get
     // n + klog(n) ,which, is better
-    // check this sol in video
+    // check this sol in video at 50min
     public static int kthSmallest(int[] arr, int l, int r, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
             return b - a;
@@ -99,51 +99,104 @@ public class Questions {
         return ans;
     }
 
-    // 692
-    public List<String> topKFrequent(String[] words, int k) {
-        HashMap<String, Integer> map = new HashMap<>();
-        for (String word : words) {
-            map.put(word, map.getOrDefault(word, 0) + 1);
-        }
-        PriorityQueue<String[]> pq = new PriorityQueue<>((a, b) -> {
-            int temp1 = Integer.parseInt(a[1]);
-            int temp2 = Integer.parseInt(b[1]);
-            if (temp1 == temp2) {
-                return a[0].compareTo(b[0]);
+    // leetcode 778
+    public int swimInWater(int[][] grid) {
+        int[][] dir = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+        int maxHeight = 0;
+        int n = grid.length;
+        int m = grid[0].length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+            int i1 = a / m, j1 = a % m;
+            int i2 = b / m, j2 = b % m;
+            return grid[i1][j1] - grid[i2][j2];
+        });
+        boolean[][] vis = new boolean[n][m];
+        pq.add(0);
+        vis[0][0] = true;
+        while (pq.size() != 0) {
+            int idx = pq.remove();
+            int r = idx / m;
+            int c = idx % m;
+            vis[r][c] = true;
+            maxHeight = Math.max(maxHeight, grid[r][c]);
+            if (r == n - 1 && c == m - 1) {
+                break;
             }
-            return temp2 - temp1;
-        });
-        map.forEach((key, value) -> {
-            String[] temp = { key, value + "" };
-            pq.add(temp);
-        });
-
-        List<String> ans = new ArrayList<>();
-        while (k-- > 0) {
-            ans.add(pq.remove()[0]);
+            for (int[] arr : dir) {
+                int newR = r + arr[0];
+                int newC = c + arr[1];
+                if (newR >= 0 && newC >= 0 && newR < n && newC < m && !vis[newR][newC]) {
+                    pq.add(newR * m + newC);
+                }
+            }
         }
-        return ans;
+        return maxHeight;
     }
 
-    // 778
-    // public int swimInWater(int[][] grid) {
-
-    // }
-
     // 1642
-    // public int furthestBuilding(int[] heights, int bricks, int ladders) {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-    // }
+        int n = heights.length;
+        for (int i = 1; i < n; i++) {
+            int currDiff = heights[i] - heights[i - 1];
+            if (currDiff > 0) {
+                pq.add(currDiff);
+
+                if (pq.size() > ladders) {
+                    bricks -= pq.remove();
+                }
+
+                if (bricks < 0)
+                    return i - 1;
+            }
+        }
+
+        return n - 1;
+    }
 
     // 632
-    // public int[] smallestRange(List<List<Integer>> nums) {
+    public int[] smallestRange(List<List<Integer>> nums) {
 
-    // }
+        int n = nums.size();
 
-    // 128
-    // public int longestConsecutive(int[] nums) {
+        // {r,c}
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            int r1 = a[0], c1 = a[1];
+            int r2 = b[0], c2 = b[1];
 
-    // }
+            return nums.get(r1).get(c1) - nums.get(r2).get(c2);
+
+        });
+
+        int maxValue = -(int) 1e9;
+
+        for (int i = 0; i < n; i++) {
+            pq.add(new int[] { i, 0 });
+            maxValue = Math.max(maxValue, nums.get(i).get(0));
+        }
+
+        int range = (int) 1e9, sp = -1, ep = -1;
+        while (pq.size() == n) {
+
+            int[] re = pq.remove();
+            int r = re[0], c = re[1], val = nums.get(r).get(c);
+
+            if (maxValue - val < range) {
+                range = maxValue - val;
+                sp = val;
+                ep = maxValue;
+            }
+
+            c++;
+            if (c < nums.get(r).size()) {
+                pq.add(new int[] { r, c });
+                maxValue = Math.max(maxValue, nums.get(r).get(c));
+            }
+        }
+
+        return new int[] { sp, ep };
+    }
 
     // 781
     // best Test case=[0,0,1,1,1,2]
@@ -171,12 +224,12 @@ public class Questions {
 
     // 1218
     // public int longestSubsequence(int[] arr, int difference) {
-        
+
     // }
 
     // 1027
     // public int longestArithSeqLength(int[] nums) {
-        
+
     // }
 
 }
